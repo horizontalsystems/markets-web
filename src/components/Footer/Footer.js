eed8f3fccf9e527f50f0898f026ac7f3aacae21d
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-05-05 16:51:20 +0600

    Design subscribe form

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,47 +8,45 @@ function Footer() {
     <footer className="Footer bg-dark py-5">
       <div className="container">
         <div className="row">
-          <div className="col-md-8">
+          <div className="col-md-7">
             <ul className="list-unstyled">
-              <li>
+              <li className="py-2">
                 <a className="text-muted text-decoration-none" href="/">Home</a>
               </li>
-              <li>
+              <li className="py-2">
                 <a className="text-muted text-decoration-none" href="/">About</a>
               </li>
-              <li>
+              <li className="py-2">
                 <a className="text-muted text-decoration-none" href="/">Contact</a>
               </li>
-              <li>
+              <li className="py-2">
                 <a className="text-muted text-decoration-none" href="/">Unstopabble Wallet</a>
               </li>
             </ul>
           </div>
 
-          <div className="col-md-4 text-muted small">
+          <div className="col-md-5 text-muted small">
             <div className="row">
               <div className="d-flex py-1">
                 <div className="w-100 pe-3">
-                  <input type="email" id="email" placeholder="Email" className="form-control" />
+                  <input type="email" id="email" placeholder="Email" className="form-control bg-tyler border" />
                 </div>
                 <div className="flex-shrink-1">
-                  <button type="submit" className="btn btn-warning">Subscribe</button>
+                  <button type="submit" className="btn btn-warning px-md-4 px-sm-2"> Subscribe</button>
                 </div>
               </div>
             </div>
             <div className="row">
               <div className="d-flex py-1 pt-2">
-                <input className="form-check-input" type="checkbox" value="1" id="flexCheckChecked" />
+                <input className="form-check-input" type="checkbox" value="1" id="flexCheckChecked" checked />
                 <label className="form-check-label ps-2" htmlFor="flexCheckChecked">
                   By signing up, you agree to Horizontal Systems Privacy Policy
                 </label>
               </div>
             </div>
             <div className="row">
-              <div className="d-flex py-1">
-                <span>
-                  Subscribe to our newsletter to get new products, guides and cheat sheets when they are published.
-                </span>
+              <div className="d-flex py-2">
+                Subscribe to our newsletter to get new products, guides and cheat sheets when they are published.
               </div>
             </div>
           </div>
