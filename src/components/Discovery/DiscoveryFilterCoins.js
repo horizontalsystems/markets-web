eaec3de6630240805241e7fa62d4585258c6a30a
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-06-01 18:01:35 +0600

    Show spinner in CoinList

--- src/components/Discovery/DiscoveryFilterCoins.js
+++ src/components/Discovery/DiscoveryFilterCoins.js
@@ -7,12 +7,13 @@ import CoinList from '../List/CoinList'
 function DiscoveryFilterCoins() {
   const dispatch = useDispatch()
   const filteredCoins = useSelector(state => selectFilteredCoins(state))
+  const filteredCoinsFetching = useSelector(state => state.discovery.filtersCoinFetching)
 
   useEffect(() => {
     dispatch(fetchFilteredCoins())
   }, [dispatch])
 
-  return <CoinList coins={filteredCoins} isFetching={!filteredCoins.length} />
+  return <CoinList coins={filteredCoins} isFetching={filteredCoinsFetching} />
 }
 
 export default DiscoveryFilterCoins
